f47de70922c481ecf6955b68a92c551acf75f624
Leandro Peres <leandroperes@protonmail.com>
2025-05-26 11:52:56 -0300

    feat: add `tailwindcss` and `daisyui` dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+
+  css: ["~/assets/css/main.css"],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
   srcDir: "src/",
+
   modules: ["@nuxt/eslint"],
 
   eslint: {
