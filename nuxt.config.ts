a3d3c4eed6e7e51826142844c3da68b640ca37e7
Leandro Peres <leandroperes@protonmail.com>
2025-05-26 11:21:32 -0300

    feat: implement `ESLint`/`lint-staged`
    Make usage of `antfu/eslint-config`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  srcDir: 'src/',
-})
+  srcDir: "src/",
+  modules: ["@nuxt/eslint"],
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+});
