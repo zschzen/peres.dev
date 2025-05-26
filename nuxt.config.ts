2e6eb3e4e62f7c190af3a00379fc9d222984e861
Leandro Peres <leandroperes@protonmail.com>
2025-05-26 18:35:00 -0300

    feat: add `@nuxt/icon` and `@nuxt/font`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      title: "Leandro Peres", // default fallback title
+      htmlAttrs: {
+        lang: "en",
+      },
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      ],
+    },
+  },
+
   css: ["~/assets/css/main.css"],
 
   vite: {
@@ -15,7 +27,12 @@ export default defineNuxtConfig({
 
   srcDir: "src/",
 
-  modules: ["@nuxt/eslint", "@vesp/nuxt-fontawesome"],
+  modules: [
+    "@nuxt/eslint",
+    "@vesp/nuxt-fontawesome",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+  ],
 
   eslint: {
     config: {
